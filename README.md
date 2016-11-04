86353c3ddfcea58c6ed86ff78818418bc4c93aa6
Eoin Bailey <Eoin Bailey>
2016-11-04 10:04:57 +0000

    initial set up

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# airBerlinTeamsFlights
+## Inspiration
+Planning a team trip can be challenging. Removing the friction of finding out how to get from your city to the team-city makes things a lot easier! Do this within slack, Microsoft Teams, Skype, or the web!
+
+## What it does
+Look for flight options from the city the team is in (or each team member) to the city everyone needs to get to. The app queries for flight combinations and returns the top options to the team without them leaving the tool they're already using to plan the trip.
+
+## How I built it
+The app is built with the Microsoft Bot Framework, LUIS.ai from Microsoft, the AirBerlin APIs, and NodeJs.
+
+## Challenges I ran into
+The bot can use natural language processing as well as directed commands. Directed commands are relatively straight forward to manage, but natural language questions require lots of training data and figuring out all the ways someone might ask even a simple question is tricky!
+
+## Accomplishments that I'm proud of
+Microsoft Teams talks to the Bot Framework, the Bot Framework talks to LUIS for natural language processing, LUIS returns to the Bot, the Bot acts on the response which leads to a query on the Air Berlin API, the response from the API provides an answer to the user. Getting each of those components to talk to each other successfully is something I'm proud of!
+
+## What I learned
+Bots need to be able to understand directed commands as well as natural language questions. Different users interact with the bot in their own way.
+
+## What's next for Air Berlin Team Flights
+Integrate a query to get airport codes for cities -- and for cities with multiple airports confirm the correct airport option.
---
+++ app.js
@@ -0,0 +1,82 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+//=========================================================
+// Bot Setup
+//=========================================================
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log("%s listening to %s", server.name, server.url); 
+});
+  
+// Create chat bot
+var connector = new builder.ChatConnector({
+    appId: "7bdd14d3-9aef-4ed0-95b1-aa05e390cb4c", //process.env.MICROSOFT_APP_ID,
+    appPassword: "hcBcXCPOvibdhjsirDW7hxj" //process.env.MICROSOFT_APP_PASSWORD
+});
+var bot = new builder.UniversalBot(connector);
+server.post("/api/messages", connector.listen());
+
+// TEST WEB CHAT:
+server.get("/", function(req, res, next) {
+  var body = "<html><iframe src='https://webchat.botframework.com/embed/airBerlinQuery?s=P-3BUxC8uDE.cwA.Pys.MV295X7WE21Tj-3Df42VO9P_yuHTTtQVw87cKnc3nUg'></iframe></html>";
+  res.writeHead(200, {
+    'Content-Length': Buffer.byteLength(body),
+    'Content-Type': 'text/html'
+  });
+  res.write(body);
+  res.end();
+  next();
+});
+
+
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+bot.dialog("/", [
+  function (session, args, next) {
+    session.beginDialog("/routeQuery");
+  }
+]);
+
+bot.dialog("/routeQuery", [
+  function (session) {
+    builder.Prompts.choice(session, "Are you feeling unwell today?", ["Yes", "No"]);
+  },
+  function (session, results) {
+    session.dialogData.feelingSick = results.response;
+    if (session.dialogData.feelingSick.entity == "Yes") {
+      // sick
+      builder.Prompts.choice(session, "Is your head or neck sore?", ["Yes", "No"]);
+    } else {
+      // not sick
+      builder.Prompts.choice(session, "Did you exercise hard yesterday?", ["Yes", "No"]);
+    }
+  },
+  function (session, results) {
+    if (session.dialogData.feelingSick.entity == "Yes") {
+      if (results.response.entity == "Yes") {
+        session.send("I don't think you should run today. If you're not feeling well for a few days I can delay your next order by a week?");
+      } else {
+        session.send("Go for a gentle run today. Make a Nut-late with the Bonk-Cup after you stretch.");
+      }
+    } else {
+      if (results.response.entity == "Yes") {
+        session.send("Go for a moderate run today. I think the Mid-Cup from your last order will really help your recovery.");
+      } else {
+        session.send("Go for a normal run today and eat the Super-Cup from your last order during your run.");
+      }
+    }
+    session.endDialog();
+  }
+]);
+
+// bot.dialog("/commands", [
+//   function(session) {
+//     builder.Prompts.choice(session, "Here are the commands you can send me:", [])
+//   }
+// ]);
