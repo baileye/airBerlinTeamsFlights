8ef906cadf1aca59bfdf618144c9628397697255
Eoin Bailey <Eoin Bailey>
2016-11-04 10:06:00 +0000

    update

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
   
 // Create chat bot
 var connector = new builder.ChatConnector({
-    appId: "7bdd14d3-9aef-4ed0-95b1-aa05e390cb4c", //process.env.MICROSOFT_APP_ID,
-    appPassword: "hcBcXCPOvibdhjsirDW7hxj" //process.env.MICROSOFT_APP_PASSWORD
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 var bot = new builder.UniversalBot(connector);
 server.post("/api/messages", connector.listen());
